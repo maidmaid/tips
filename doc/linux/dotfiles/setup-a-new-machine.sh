9e80d57df9f12ccf83fab08ac383c6ef53df7891
Dany Maillard <danymaillard93b@gmail.com>
2016-09-25 22:19:40 +0200

    Add git files in dotfiles section

--- doc/linux/dotfiles/setup-a-new-machine.sh
+++ doc/linux/dotfiles/setup-a-new-machine.sh
@@ -2,3 +2,7 @@ echo "do not run this script in one go. hit ctrl-c NOW"
 read -n 1
 
 ln -fs "$(pwd)/.bash_aliases" ~/.bash_aliases
+ln -fs "$(pwd)/.gitattributes" ~/.gitattributes
+ln -fs "$(pwd)/.gitconfig" ~/.gitconfig
+ln -fs "$(pwd)/.gitconfig.local" ~/.gitconfig.local
+ln -fs "$(pwd)/.gitignore" ~/.gitignore
